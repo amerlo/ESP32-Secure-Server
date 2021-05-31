4cd70c3b21d09af66c38e3d4bf07c257ad5f1a82
amerlo <amerloserrano@gmail.com>
2021-05-31 19:54:41 +0200

    ADD Web page

---
+++ data/handlers.js
@@ -0,0 +1,46 @@
+$(function() {
+    $.get("api/status", function(data) {
+        jQuery(data).each(function(i, relay) {
+            var checked = relay.value ? "checked" : "";
+            var item = `<li id="${relay.id}" class="list-group-item">
+                <div class="row">
+                    <div class="col-10">
+                        <label class="form-check-label">${relay.name} - GPIO ${relay.gpio}</label>
+                    </div>
+                    <div class="col-2 d-flex justify-content-center">
+                        <div class="spinner visually-hidden" style="width: 30px;">
+                            <div class="spinner-border spinner-border-sm text-secondary" role="status"></div>
+                        </div>
+                        <div class="form-check form-switch" style="margin-bottom: 0;">
+                            <input class="form-check-input shadow-none" type="checkbox" id="${relay.id}" onchange="handleChange(event)" ${checked} />
+                        </div>
+                    </div>
+                </div>
+            </li>`;
+
+            $("#relays").append(item);
+        });
+
+        $("#loadingSpinner").addClass("visually-hidden");
+        $("#relaysCard").removeClass("visually-hidden");
+    });
+});
+
+function handleChange(e) {
+    var relayId = e.target.id;
+    var relayValue = e.target.checked;
+
+    $("li#"+relayId+" .form-switch").addClass("visually-hidden");
+    $("li#"+relayId+" .spinner").removeClass("visually-hidden");
+    
+    $.post("api/update", `{ "id": ${relayId}, "value": ${relayValue} }`)
+        .fail(function() {
+            $("li#"+relayId+" input").prop("checked", !relayValue);
+            $('.toast').toast('show');
+        })
+        .always(function() {
+            $("li#"+relayId+" .spinner").addClass("visually-hidden");
+            $("li#"+relayId+" .form-switch").removeClass("visually-hidden");
+            $("li#"+relayId+" input").blur();
+        });
+}
