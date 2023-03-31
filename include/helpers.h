3e4742a1fe7ec0d5d7e21fb82bd29f95fa30b68a
amerlo <amerloserrano@gmail.com>
2023-03-31 11:10:26 +0200

    UPDATE Move helpers.h to include folder and add DDNS update task definition

--- include/helpers.h
+++ include/helpers.h
@@ -6,10 +6,13 @@
 #include <WiFi.h>
 #include <EasyDDNS.h>
 
+using namespace httpsserver;
+
 void spiffsInitialization();
-void setResponseContent(httpsserver::HTTPResponse * res, String filePath);
+void setResponseContent(HTTPResponse * res, String filePath);
 void wifiConnection(const char * ssid, const char * password);
 void dynamicDNSInitialization(const char * domain, const char * token);
-void dynamicDNSUpdate(int seconds);
+void dynamicDNSUpdate(int interval);
+void dynamicDNSUpdateLoop(unsigned long * lastUpdateTime, int interval);
 
 #endif
